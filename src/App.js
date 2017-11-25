fc121d79b9774f0f75e2b3697c246370c7993aaa
Ben Rudolph <rudolphben@gmail.com>
2017-11-25 16:01:33 -0600

    Slow example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactMap from 'react-mapbox-gl';
+import { Marker, Layer, Source, Popup } from "react-mapbox-gl"
 
 const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w";
 const style = "mapbox://styles/mapbox/streets-v9";
@@ -13,13 +14,37 @@ const mapStyle = {
   width: '100vw'
 };
 
+class MyMarker extends Component {
+  render() {
+
+    const center = [-95.3698, 29.7604]
+    const imageUrl = 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/apple/114/smiling-face-with-sunglasses_1f60e.png';
+    const newCoord = [(Math.random() * 0.06) + center[0], center[1] + (Math.random() * 0.06)];
+    return (
+      <Marker coordinates={newCoord} >
+        <img src={imageUrl} width="50" height="50" />
+      </Marker>
+    )
+  }
+}
+
 class App extends Component {
   render() {
+
+    let markers = []
+    const center = [-95.3698, 29.7604]
+
+    for (let i = 0; i < 1000; i++) {
+      markers.push(<MyMarker key={i} />)
+    }
     return (
       <Map
         style={style}
         containerStyle={mapStyle}
-      />
+        center={center}
+      >
+        {markers}
+      </Map>
     );
   }
 }
